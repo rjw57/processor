985c5371cc783df110464d75e43cb604505a8df8
Rich Wareham <rich@richwareham.com>
2022-10-19 19:42:45 +0100

    add ram module

--- verilog/ram-tb.sv
+++ verilog/ram-tb.sv
@@ -2,21 +2,23 @@
 
 `TBPROLOGUE
 
-localparam ROM_READ_DELAY = 150;
+localparam RAM_READ_DELAY = 45, RAM_WRITE_DELAY = 45;
 
 reg [14:0] A;
 reg WE_bar, OE_bar, CS_bar;
 wire [7:0] Q;
+reg [7:0] D;
 
 // Device under test
-rom #(
-  .ROM_CONTENTS ("../roms/popcount.mem"),
-  .READ_DELAY (ROM_READ_DELAY)
+ram #(
+  .READ_DELAY (RAM_READ_DELAY),
+  .WRITE_DELAY (RAM_WRITE_DELAY)
 ) dut (
   .A(A),
   .WE_bar(WE_bar),
   .OE_bar(OE_bar),
   .CS_bar(CS_bar),
+  .D(D),
   .Q(Q)
 );
 
@@ -27,12 +29,27 @@ rom #(
 
   `TBDELAY(2)
   A = 16'h0101;
-  OE_bar = 1'b0;
-  CS_bar = 1'b0;
+  D = 8'h8A;
 
-  #(ROM_READ_DELAY)
+  CS_bar = 1'b0;
+  WE_bar = 1'b0;
+  #(RAM_WRITE_DELAY)
+  WE_bar = 1'b1;
+
   `TBDELAY(2)
-  `TBASSERT(Q === 8'h02, "basic read");
+  A = 16'h1010;
+  D = 8'hA8;
+
+  WE_bar = 1'b0;
+  #(RAM_WRITE_DELAY)
+  WE_bar = 1'b1;
+
+  `TBDELAY(2)
+  A = 16'h0101;
+  OE_bar = 1'b0;
+  #(RAM_READ_DELAY)
+  `TBDELAY(2)
+  `TBASSERT(Q === 8'h8A, "basic read");
 
   `TBDELAY(2)
   CS_bar = 1'b0;
@@ -47,14 +64,15 @@ rom #(
   `TBASSERT(Q === 8'bZ, "high-Z via CS");
 
   `TBDELAY(2)
-  A = 16'h7FFF;
+  A = 16'h1010;
   WE_bar = 1'b1;
   OE_bar = 1'b0;
   CS_bar = 1'b0;
 
   `TBDELAY(2)
-  `TBASSERT(Q !== 8'h0F, "propagation delay");
+  `TBASSERT(Q !== 8'hA8, "propagation delay");
 
-  #ROM_READ_DELAY
-  `TBASSERT(Q === 8'h0F, "read from last byte");
+  #RAM_READ_DELAY
+  `TBASSERT(Q === 8'hA8, "read");
 `TBEND
+
---
+++ verilog/ram.v
@@ -0,0 +1,38 @@
+// RAM module
+module ram
+#(
+  parameter
+  ADDR_WIDTH = 15,
+  DELAY_RISE = 0,
+  DELAY_FALL = 0,
+  READ_DELAY = 0,
+  WRITE_DELAY = 0
+)
+(
+  input [ADDR_WIDTH-1:0] A,
+  input WE_bar,
+  input OE_bar,
+  input CS_bar,
+  input [7:0] D,
+  output [7:0] Q
+);
+
+reg [7:0] Q_out;
+reg [7:0] data[0:2**ADDR_WIDTH-1];
+
+assign #(DELAY_RISE, DELAY_FALL) Q = (~OE_bar & WE_bar * ~CS_bar) ? Q_out : 'bZ;
+
+always @(A)
+begin
+  #READ_DELAY Q_out = data[A];
+end
+
+always @(WE_bar or OE_bar or CS_bar or D)
+begin
+  if(~WE_bar & OE_bar & ~CS_bar)
+  begin
+    #WRITE_DELAY data[A] = D;
+  end
+end
+
+endmodule
